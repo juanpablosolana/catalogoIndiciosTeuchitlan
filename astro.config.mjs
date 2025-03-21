b50f290512363e205249f30353b143b56e23f60a
Juan Pablo Solana Ortiz <juanpablosolana@gmail.com>
2025-03-20 22:22:09 -0600

    Añade workflow para desplegar en GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://juanpablosolana.github.io', // Reemplaza TU-USUARIO con tu nombre de usuario de GitHub
+  base: '/catalogoIndiciosTeuchitlan', // Reemplaza con el nombre de tu repositorio (omítelo si es username.github.io)
+  output: 'static', // Asegúrate de que sea static para GitHub Pages
+});
